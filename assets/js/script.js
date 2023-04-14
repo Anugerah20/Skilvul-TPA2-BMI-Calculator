e4ecf402a698e6366b5088f234a581d416bd707d
Nabil Anugerah <nabilpangestu123@gmail.com>
2023-04-14 15:01:07 +0700

    update script js

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,7 +36,10 @@ function checkYourBMI() {
                categories = 'Obesity ';
           }
 
-          document.getElementById('show').innerHTML = "Your BMI is <b>" + bmi + "</b> which means you are <b>" + categories + "</b>";
+          let showText = document.getElementById('show').innerHTML = `
+          <p style='color: #FFFFFF; background: #6558f5; padding: 1.5rem;border-radius: 8px'>
+          Your BMI is <b>${bmi}</b> which means you are <b>${categories}</b>
+          </p>`;
      }
 }
 
